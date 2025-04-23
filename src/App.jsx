0f6af566784c73cc280a5a1a06a4cabae08cc8b8
LindaTancini <lindatancini95@gmail.com>
2025-04-23 15:41:05 +0200

    feat: add provider

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import About from "./pages/About";
 import PostsPage from "./pages/PostsPage";
 import DefaultLayout from "./layouts/DefaultLayout";
 import PostsDetail from "./pages/PostsDetail";
+import { PostsProvider } from "./contexts/PostsContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          {/* CONFIGURO LE ROTTE */}
-          <Route Component={DefaultLayout}>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/posts" element={<PostsPage />} />
-            <Route path="/posts/:id" element={<PostsDetail />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <PostsProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* CONFIGURO LE ROTTE */}
+            <Route Component={DefaultLayout}>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/posts" element={<PostsPage />} />
+              <Route path="/posts/:id" element={<PostsDetail />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PostsProvider>
     </>
   );
 }
