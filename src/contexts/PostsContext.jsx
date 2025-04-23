f86bfb038dd5718b19dd0230ce53d0f5941396e6
LindaTancini <lindatancini95@gmail.com>
2025-04-23 15:25:48 +0200

    feat: create provider

--- src/contexts/PostsContext.jsx
+++ src/contexts/PostsContext.jsx
@@ -1,5 +1,32 @@
 //IMPORTAZIONI
-import { createContext } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
+import axios from "axios";
 
 //CREIAMO IL CONTESTO
 const PostsContext = createContext();
+//CREO UN PROVIDER CHE FORNIRA' I DATI DEI POST
+function PostsProvider({ children }) {
+  //AGGIUNGO LE VARIABILI DI STATO
+  const [posts, setPosts] = useState([]);
+  //FACCIO LA CHIAMATA AXIOS
+  const endpoint = "https://jsonplaceholder.typicode.com/posts";
+  useEffect(() => {
+    axios
+      .get(endpoint)
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.error("Errore:", err));
+  }, []);
+  return (
+    //FORNIAMO I DATI AI POST A TUTTI I COMPONENTI FIGLI
+    <PostsContext.Provider value={{ posts }}>{children}</PostsContext.Provider>
+  );
+}
+
+//CREO UN HOOK PERSONALIZZATO PER CONSUMARE IL CONTESTO
+function usePosts() {
+  const context = useContext(PostsContext);
+  return context;
+}
+
+//ESPORTIAMO IL PROVIDER E L'HOOK
+export { PostsContext, usePosts };
